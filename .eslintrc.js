8f043dde3ea12d2ea5788e26fa39e756cab80dcd
maojie <j.mao@maimemo.com>
2021-05-01 23:36:06 +0800

    feat(index): test search api

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true
+  },
+  extends: [
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 12
+  },
+  rules: {
+    'object-curly-spacing': ['error', 'always'],
+    'space-before-function-paren': ['error', {
+      anonymous: 'always',
+      named: 'never',
+      asyncArrow: 'always'
+    }]
+  }
+}
---
+++ index.js
@@ -0,0 +1,43 @@
+#! /usr/bin/env node
+
+const { program } = require('commander')
+const JishoApi = require('unofficial-jisho-api')
+const jisho = new JishoApi()
+const Table = require('cli-table')
+
+program.option('-p, --phrase', 'search for phrase')
+  .option('-en, --english', 'search for phrase of english word')
+
+async function main() {
+  program.parse(process.argv)
+  const options = program.opts()
+  let search = program.args.join().trim()
+  if (options.english) {
+    search = `"${search}"`
+  }
+  console.log(search)
+  const result = await jisho.searchForPhrase(search)
+
+  const header = ['word', 'reading', 'is_common', 'jlpt']
+  const table = new Table({
+    head: header,
+    colWidths: [20, 20, 20, 20],
+    colAligns: ['left'],
+    style: { 'padding-right': 0 }
+  })
+
+  for (const item of result.data) {
+    if (item.japanese && Array.isArray(item.japanese)) {
+      const jlpt = item.jlpt && Array.isArray(item.jlpt)
+        ? item.jlpt.join(' ')
+        : ''
+      for (const jp of item.japanese) {
+        table.push([jp.word || '', jp.reading || '', item.is_common || '', jlpt])
+      }
+    }
+  }
+  console.log(table.toString())
+  // console.log(JSON.stringify(result.data[0], null, 2))
+}
+
+main().catch(console.error)
